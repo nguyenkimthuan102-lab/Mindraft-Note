745ff9d0f70079fa6150a23afcde053509dbd26e
Nguyen Kim Thuan <nguyenkimthuan102@gmail.com>
2026-04-27 23:30:45 +0700

    Khởi tạo cấu trúc dự án

---
+++ frontend/app/(main)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Giao diện nền tảng đã sẵn sàng</Text>
+    </View>
+  );
+}
